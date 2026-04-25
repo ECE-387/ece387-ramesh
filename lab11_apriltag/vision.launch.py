d5d56b03b0bec0372b929f13cfd53c254e4c7ec9
Ramya Ramesh <c27Ramya.Ramesh@afacademy.af.edu>
2026-04-24 21:01:14 -0600

    lab 11

---
+++ lab11_apriltag/vision.launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('lab11_apriltag')
+    config_dir = os.path.join(pkg_share, 'config')
+
+    stop_detector_model = os.path.join(config_dir, 'stop_detector.svm')
+    camera_info_path = os.path.join(config_dir, 'default_cam.yaml')
+    camera_info_url = 'file://' + camera_info_path
+
+    return LaunchDescription([
+        Node(
+            # TODO: Complete this function call
+            package = "lab10_cv",
+            executable = "stop_detector",
+            name = "stop_detector",
+            output = "screen",
+            arguments=['-d', stop_detector_model],
+        ),
+        Node(
+            # TODO: Complete this function call
+            package = "usb_cam",
+            executable = "usb_cam_node_exe",
+            name = "usb_cam",
+            output = "screen",
+            parameters=[{
+                'video_device': '/dev/video0',
+                'framerate': 15.0,
+                'camera_info_url': camera_info_url
+            }]
+        ),
+        Node(
+            # TODO: Complete this function call
+            package = "lab11_apriltag",
+            executable = "apriltag",
+            name = "apriltag_node"
+            output = "screen",
+            parameters=[{
+                'camera_info_file': camera_info_path
+            }]
+        ),
+    ])
